c1c202c62f74999ab582f7fbed4dee1c170f981e
Gerald Kyro Villanueva Tungol <Lynxvillicana@gmail.com>
2022-10-25 18:58:19 -0700

    testing

--- A67.hpp
+++ A67.hpp
@@ -4,6 +4,24 @@
 #include <iostream>
 using namespace std;
 
+void swap(int &n, int &s){
+    int temp;
+    temp = n;
+    n = s;
+    s = temp;
+}
+void swap(int &n1, int &n2, int &n3){
+    swap(n1, n2);
+    swap(n1, n3);
+}
+void swap(int &na, int &nb, int &nc, int &nd){
+    swap(na, nb);
+    swap(nb, nc);
+    swap(nc, nd);
+    swap(nd, na);
+}
+
+
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
